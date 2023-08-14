5e8b35d195716a961aec2a19a8d7d603046f7168
Varun <varunvg0011@gmail.com>
2023-08-14 11:57:14 +0530

    Learning testing

---
+++ Sparky/Calculator.cs
@@ -0,0 +1,47 @@
+﻿namespace Sparky
+{
+    public class Calculator
+    {
+
+
+        public List<int> NumberRange { get; set; } = new List<int>();
+        public int AddTwoNo(int a, int b)
+        {
+            return a + b;
+        }
+
+
+        public bool IfSameNo(int a, int b)
+        {
+            return a == b;
+        }
+
+        public bool IsOdd(int a)
+        {
+            
+            return a % 2 != 0;
+            
+        }
+
+
+        public double AddTwoDoubleNos(double a, double b)
+        {
+            return a + b;
+        }
+
+
+        public List<int> GetOddNumbersFromRange(int a, int b)
+        {
+            NumberRange.Clear();
+            for (int i = a; i < b; i++)
+            {
+                if (i % 2 != 0)
+                {
+                    NumberRange.Add(i);
+                }
+            }
+            return NumberRange;
+        }
+
+    }
+}
---
+++ Sparky/Customer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sparky
+{
+    public class Customer
+    {
+        public string GreetMsg { get; set; }
+        public string CustFullName(string firstName, string lastName)
+        {
+            GreetMsg = firstName + " " + lastName;
+            return GreetMsg;
+        }
+    }
+}
